b33a9229a9dfebe9964a0cff08ba6acb573e981b
Brian Ritchie <sayhello@brianritchie.me>
2017-08-10 15:33:27 +0800

    Took a break - really starting now

--- set_1/challenge_4/solution.go
+++ set_1/challenge_4/solution.go
@@ -3,8 +3,33 @@ package main
 import (
 
 	"fmt"
+	"bufio"
+	"os"
 )
 
+// This function will check for errors and output if one occurs
+func check(e error) {
+
+	if e != nil {
+		panic(e)
+	}
+	
+}
+
 func main() {
-	fmt.Println("This file will find the solution to the challenge")
+	
+	//Open the file and check if there are any errors that occur
+	source_file, err := os.Open("4.txt")
+	check(err)
+
+	// Lets now read the file line by line
+	file_scanner := bufio.NewScanner(source_file)
+	for file_scanner.Scan() {
+		fmt.Println(file_scanner.Text())
+		
+	}
+
+	
+	// Close the file after usage
+	source_file.Close()
 }
