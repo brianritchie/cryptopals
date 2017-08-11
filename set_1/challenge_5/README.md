5bd65011fa14894d940137c795bc2aceaa44612a
Brian Ritchie <sayhello@brianritchie.me>
2017-08-11 20:09:36 +0800

    Cryptopals Set_1 Challenge_5 Complete - Solved using matching positions

---
+++ set_1/challenge_5/README.md
@@ -0,0 +1,9 @@
+# Challenge 5 Write-up
+
+The challenge specifies we need to take a string given and encrypt it using the key "ICE" and the repeating-key XOR method.
+
+---
+
+This is actually quite straightforward ot achieve given the previous challenges. The only trick was to make sure the position of the bytes in the key matched the position evaluated fro the source string. 
+
+QED
--- set_1/challenge_5/solution.go
+++ set_1/challenge_5/solution.go
@@ -14,12 +14,36 @@ func main() {
 	
 	store := make([]byte, len(source_string))
 
-	key_one := 0
-	key_two := 1
-	key_three := 3
+	pos_one := 0
+	pos_two := 1
+	pos_three := 2
+
+	key := "ICE"
 
 	for a := 0; a < len(source_string); a++ {
 
+		if a == pos_one {
+			
+			store[a] = source_string[a] ^ key[0]
+			pos_one = pos_one + 3
+			
+		} else if a == pos_two {
+			
+			store[a] = source_string[a] ^ key[1]
+			pos_two = pos_two + 3
+			
+		} else if a == pos_three {
+			
+			store[a] = source_string[a] ^ key[2]
+			pos_three = pos_three + 3
+			
+		}
+
 	}
+
+	fmt.Println("The original string to be encrypted is -->", source_string)
+
+	fmt.Println("The solution is as follows -->")
+	fmt.Printf("%x\n", string(store))
 
 }
